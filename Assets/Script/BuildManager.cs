102dd673b6be2e5f879a2a2f482d758dc3fb0732
g64160023 <auliapadli12@yahoo.com>
2019-05-11 20:58:47 +0700

    tambah class node + buildmanager, tapi posisi masih ngacak

---
+++ Assets/Script/BuildManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public static BuildManager instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            return;
+        }
+        instance = this;
+
+    }
+    public GameObject standardAnimalPrefab;
+
+    void Start()
+    {
+        animalToSpawn = standardAnimalPrefab;
+    }
+
+    private GameObject animalToSpawn;
+    public GameObject GetAnimalToSpawn()
+    {
+        return animalToSpawn;
+    }
+    // Start is called before the first frame update
+
+}
---
+++ Assets/Script/Nodes.cs
@@ -0,0 +1,38 @@
+﻿
+using UnityEngine;
+
+public class Nodes : MonoBehaviour
+{
+    private GameObject animal;
+    public Color hoverColor;
+    private Renderer Rend;
+    private Color startColor;
+    public Quaternion rotationOffset;
+    public Vector3 positionOffset;
+
+    void Start()
+    {
+        Rend = GetComponent<Renderer>();
+        startColor = Rend.material.color;
+    }
+
+    private void OnMouseEnter()
+    {
+        Rend.material.color = hoverColor;
+    }
+
+    private void OnMouseDown()
+    {
+        if (animal != null)
+        {
+            return;
+        }
+        GameObject animalToSpawn = BuildManager.instance.GetAnimalToSpawn();
+        animal = (GameObject)Instantiate(animalToSpawn, transform.position+positionOffset, rotationOffset);
+    }
+
+    private void OnMouseExit()
+    {
+        Rend.material.color = startColor;
+    }
+}
